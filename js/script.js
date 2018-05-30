093785008c1d56d7472ff5a76257d88802a83731
Muatasim Qazi <muatasimqazi@gmail.com>
2018-05-29 20:50:42 -0700

    fixed projects

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+      
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#scroll').fadeIn();
+      } else {
+        $('#scroll').fadeOut();
+      }
+    });
+
+    $('#scroll').click(function () {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 800);
+      return false;
+    })
+  });
